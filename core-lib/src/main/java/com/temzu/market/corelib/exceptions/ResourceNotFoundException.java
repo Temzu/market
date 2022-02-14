8faf603a5ece560436c9c887585cea275e85d404
Temzu <40913z@mail.ru>
2022-02-14 20:05:03 +0300

    redesign ResourceNotFoundException

--- core-lib/src/main/java/com/temzu/market/corelib/exceptions/ResourceNotFoundException.java
+++ core-lib/src/main/java/com/temzu/market/corelib/exceptions/ResourceNotFoundException.java
@@ -2,34 +2,38 @@ package com.temzu.market.corelib.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
 
-  private static final String ID = "id";
-  private static final String LOGIN = "login";
-  private static final String NAME = "name";
-  private static final String TITLE = "TITLE";
+  private static final String NOT_FOUND_BY = "%s not found by [%s]: %s";
 
-  private static final String NOT_FOUND_BY = "%s with %s %s not found";
-
-  private ResourceNotFoundException(String message) {
-    super(message);
+  private ResourceNotFoundException(Class<?> entityClass, String type, String reason) {
+    super(String.format(NOT_FOUND_BY, entityClass.getSimpleName(), type, reason));
   }
 
   public static ResourceNotFoundException byId(Long id, Class<?> entityClass) {
-    return new ResourceNotFoundException(
-        String.format(NOT_FOUND_BY, entityClass.getSimpleName(), ID, id));
+    return new ResourceNotFoundException(entityClass, Type.ID.type, id.toString());
   }
 
   public static ResourceNotFoundException byName(String name, Class<?> entityClass) {
-    return new ResourceNotFoundException(
-        String.format(NOT_FOUND_BY, entityClass.getSimpleName(), NAME, name));
+    return new ResourceNotFoundException(entityClass, Type.NAME.type, name);
   }
 
   public static ResourceNotFoundException byTitle(String title, Class<?> entityClass) {
-    return new ResourceNotFoundException(
-        String.format(NOT_FOUND_BY, entityClass.getSimpleName(), TITLE, title));
+    return new ResourceNotFoundException(entityClass, Type.TITLE.type, title);
   }
 
   public static ResourceNotFoundException byLogin(String login, Class<?> entityClass) {
-    return new ResourceNotFoundException(
-        String.format(NOT_FOUND_BY, entityClass.getSimpleName(), LOGIN, login));
+    return new ResourceNotFoundException(entityClass, Type.LOGIN.type, login);
+  }
+
+  private enum Type {
+    ID("ID"),
+    LOGIN("LOGIN"),
+    NAME("NAME"),
+    TITLE("TITLE");
+
+    private final String type;
+
+    Type(String type) {
+      this.type = type;
+    }
   }
 }
