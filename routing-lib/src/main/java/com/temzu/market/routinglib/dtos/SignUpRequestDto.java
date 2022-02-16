ed8384d25179d744d483f233bb0781fabde79e3d
Temzu <40913z@mail.ru>
2022-02-16 18:11:54 +0300

    added test for auth service

--- routing-lib/src/main/java/com/temzu/market/routinglib/dtos/SignUpRequestDto.java
+++ routing-lib/src/main/java/com/temzu/market/routinglib/dtos/SignUpRequestDto.java
@@ -1,10 +1,14 @@
 package com.temzu.market.routinglib.dtos;
 
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class SignUpRequestDto {
 
+  @NonNull
   private String login;
+
+  @NonNull
   private String password;
 }
