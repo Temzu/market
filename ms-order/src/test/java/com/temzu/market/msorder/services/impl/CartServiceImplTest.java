4e568ec99b80f00f9e6c2ca1e281245255bb1dcc
Temzu <40913z@mail.ru>
2022-02-23 23:01:45 +0300

    init tests for CartService

---
+++ ms-order/src/test/java/com/temzu/market/msorder/services/impl/CartServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.temzu.market.msorder.services.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CartServiceImplTest {
+
+  @Test
+  void findCartByUuid() {
+  }
+
+  @Test
+  void createCartForUser() {
+  }
+
+  @Test
+  void addToCart() {
+  }
+
+  @Test
+  void clearCart() {
+  }
+}
