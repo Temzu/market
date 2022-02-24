eed5ee2398640a601ed9a87d7f15e2ce558f6150
Temzu <40913z@mail.ru>
2022-02-24 23:29:46 +0300

    init tests for CartControllerTest

---
+++ ms-order/src/test/java/com/temzu/market/msorder/controllers/CartControllerTest.java
@@ -0,0 +1,24 @@
+package com.temzu.market.msorder.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CartControllerTest {
+
+  @Test
+  void findCurrentCart() {
+  }
+
+  @Test
+  void createNewCart() {
+  }
+
+  @Test
+  void addProductToCart() {
+  }
+
+  @Test
+  void clearCart() {
+  }
+}
