51a7df9e4f719e90ac5897985b19852eeb71e82a
Temzu <40913z@mail.ru>
2022-02-25 19:05:17 +0300

    init tests for CartControllerTest

---
+++ core-lib/src/test/java/com/temzu/market/corelib/services/impl/TokenServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.temzu.market.corelib.services.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TokenServiceImplTest {
+
+  @Test
+  void generateToken() {
+  }
+
+  @Test
+  void parseToken() {
+  }
+
+  @Test
+  void getUserId() {
+  }
+}
